03a37f86ba76882f84473fb2a2859569b0360108
bhanvitech <dudejabhanvi@gmail.com>
2023-07-09 00:04:10 +0530

    test push

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from './components';
 
 const App = () => {
+  //test
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
@@ -22,6 +33,6 @@ const App = () => {
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
